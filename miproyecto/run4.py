6da78fa4805581fef0bf67480de7b18a0b62a0ba
mrnarvaez3 <robertoc_98_2009@hotmail.com>
2019-05-02 11:43:09 -0500

    subiendo-run4-con-comentarios

--- miproyecto/run4.py
+++ miproyecto/run4.py
@@ -10,7 +10,7 @@ Si el estudiante tiene una modalidad a distancia el numero de ciclos a seguir es
 De lo contrario debera seguir 8 ciclos.
 obtener el costo final de la carrera para un estudiante
 """
-
+#  Ingreso de datos
 modalidad = input("Ingrese su modalidad\n1.-Distancia \n2.-Presencial \n")
 
 modalidad = int(modalidad)
@@ -20,13 +20,13 @@ edad =  input("Ingrese su edad: ")
 edad = int(edad)
 
 
-
+#  Calcular precio por modalidad
 if (modalidad == 1):
 	precio = 1200 * 10
 else:
 	precio = 1200 * 8
 
-
+#  Calcular precio del seguro
 if (edad <= 20) and (modalidad == 1):
 	seguro = 100 * 10
 else:
@@ -39,8 +39,8 @@ else:
 	if(edad >20) and (modalidad == 2):
 		seguro = 150*8
 
-
+#  Calcular precio final a pagar
 preciofinal = int((precio + seguro))
-
+#  Salida de datos
 print("El precio a final a pagar es de: %d$" % preciofinal)		
 
